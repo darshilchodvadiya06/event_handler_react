30a88e1498f5429be7774aeb1dd464e7d4922886
Darshil-Chodvadiya <darshilchodvadiya59@gmail.com>
2023-08-29 15:09:48 +0530

    Event handler in React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Login from './Component/Login';
+import Signup from './Component/Signup';
+import Home from './Component/Home';
+import ForgotPass from './Component/ForgotPass';
+import Medicine from './Component/Medicine';
+import PasswordComform from './Component/PasswordComform';
+import PageNotFound from './Component/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/ForgotPass' element={<ForgotPass />} />
+          <Route path='/Medicine' element={<Medicine />} />
+          <Route path='/PasswordComform' element={<PasswordComform />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
